8972872b6eab7aff982b04202d7de46537c46620
halfpasta <christian.george360@gmail.com>
2019-12-26 18:58:27 -0500

    creating aggregated results

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
+let aggregatedResults = [];
+
+
+// for (let counter=1; counter<17599; counter++) {
+for (let counter=1; counter<10; counter++) {
 var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://rawg-video-games-database.p.rapidapi.com/games",
+	"url": "https://rawg-video-games-database.p.rapidapi.com/games?page="+counter,
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
@@ -10,5 +15,13 @@ var settings = {
 }
 
 $.ajax(settings).done(function (response) {
-	console.log(response);
+	// console.log(response);
+	// const randomImgId = Math.floor(Math.random() * (response.results.length -1)) + 1 ;
+	// console.log('-->', response.results[randomImgId].background_image);
+	for (let i=0; i<response.results.length; i++) {
+		// console.log(response.results[i]);
+		aggregatedResults.push(response.results[i]);
+	}
 });
+}
+console.log(aggregatedResults);
