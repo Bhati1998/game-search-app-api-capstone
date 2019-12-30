826bc3a620e589f8439c8c85f35b43469fc6de69
halfpasta <christian.george360@gmail.com>
2019-12-29 20:29:06 -0500

    grey box wireframe

--- scripts.js
+++ scripts.js
@@ -2,26 +2,26 @@ let aggregatedResults = [];
 
 
 // for (let counter=1; counter<17599; counter++) {
-for (let counter=1; counter<10; counter++) {
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://rawg-video-games-database.p.rapidapi.com/games?page="+counter,
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
-		"x-rapidapi-key": "54602e4456msh6e18ebcdf2c7d3fp1bb6e2jsn9a45a973098f"
+for (let counter = 1; counter < 2; counter++) {
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://rawg-video-games-database.p.rapidapi.com/games?page=" + counter,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+			"x-rapidapi-key": "54602e4456msh6e18ebcdf2c7d3fp1bb6e2jsn9a45a973098f"
+		}
 	}
-}
 
-$.ajax(settings).done(function (response) {
-	// console.log(response);
-	// const randomImgId = Math.floor(Math.random() * (response.results.length -1)) + 1 ;
-	// console.log('-->', response.results[randomImgId].background_image);
-	for (let i=0; i<response.results.length; i++) {
-		// console.log(response.results[i]);
-		aggregatedResults.push(response.results[i]);
-	}
-});
+	$.ajax(settings).done(function (response) {
+		// console.log(response);
+		// const randomImgId = Math.floor(Math.random() * (response.results.length -1)) + 1 ;
+		// console.log('-->', response.results[randomImgId].background_image);
+		for (let i = 0; i < response.results.length; i++) {
+			// console.log(response.results[i]);
+			aggregatedResults.push(response.results[i]);
+		}
+	});
 }
 console.log(aggregatedResults);
